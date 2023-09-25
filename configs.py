de6ed0bd84b923b122e0fc42c1270cc951f8d1f0
JayR1213 <111758139+JayR1213@users.noreply.github.com>
2023-09-25 18:17:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,6 +12,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5804202410"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://jayeshdb:bQmBrMvsaOSGb5dZ@cluster0.moiu8ck.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "JRBotz")
+	UPDATES_CHANNEL1 = os.environ.get("UPDATES_CHANNEL1", "Mp5moviez")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
