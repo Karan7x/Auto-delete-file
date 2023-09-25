ad43a25b414c051a91f4c5033a63a3adbd5c819a
JayR1213 <111758139+JayR1213@users.noreply.github.com>
2023-09-25 21:36:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,16 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "29627910"))
-	API_HASH = os.environ.get("API_HASH", "ef526b2a546ee6795514226f853d7ff0")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5631940394:AAHCZEeZd7V2B7_A7xzVEtJk_wEj9qYqrBo")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "FilesStores_Bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001332248654"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5804202410"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://jayeshdb:bQmBrMvsaOSGb5dZ@cluster0.moiu8ck.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "JRBotz")
-	UPDATES_CHANNEL1 = os.environ.get("UPDATES_CHANNEL1", "Mp5moviez")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	API_ID = int(os.environ.get("API_ID", ""))
+	API_HASH = os.environ.get("API_HASH", "")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6617523768:AAHgSIc1nS0EA8RDbIHD2FZvdPkGhmJyVMM")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "FilesharinG_7xBoT")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001884933078"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1773110781"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dasha:dasha7x@cluster0.kipxnlu.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Movies7x")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001968206800")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
@@ -31,17 +30,17 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @TechnoJay_bot
+🧑🏻‍💻 **Developer:** @ShinobiUnder
 
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @TechnoJay_bot
+🧑🏻‍💻 **Developer:** @ShinobiUnder
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://t.me/TechnoJay_bot) (PayPal)
+[Donate Now](https://t.me/ShinobiUnder)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
